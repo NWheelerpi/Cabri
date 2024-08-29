cc51a674aa7e37e0e8dfc8cbc0ff3f0b7d2b4d8d
NWheelerpi <156547753+NWheelerpi@users.noreply.github.com>
2024-08-29 15:25:57 +0100

    s

--- Code.py
+++ Code.py
@@ -5,6 +5,4 @@ import streamlit as st
 #parameters = np.genfromtxt('https://github.com/NWheelerpi/Cabri/blob/20f70220d87bd70c4e4bc25df4ae63adb4620f9c/Test.txt', delimiter='|')
 parameters = np.genfromtxt('https://raw.githubusercontent.com/NWheelerpi/Cabri/main/Parameters.txt.', delimiter='|',dtype=str)
 #parameters = open('https://github.com/NWheelerpi/Cabri/blob/a046b44fc315f65b8cebdb2e7d7a713d1c2d9b28/Parameters.txt.','r')
-for i in parameters:
-    for j in i:
-        st.markdown(j)
+st.markdown(parameters[0])
