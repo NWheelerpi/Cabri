ceab18fe292747d553331892f3581a47bd4ddfac
NWheelerpi <156547753+NWheelerpi@users.noreply.github.com>
2024-08-29 14:45:56 +0100

    d

--- Code.py
+++ Code.py
@@ -1,4 +1,4 @@
 import numpy as np
-https://github.com/NWheelerpi/Cabri/blob/a046b44fc315f65b8cebdb2e7d7a713d1c2d9b28/Parameters.txt.
+#https://github.com/NWheelerpi/Cabri/blob/a046b44fc315f65b8cebdb2e7d7a713d1c2d9b28/Parameters.txt.
 parameters = np.genfromtxt('https://github.com/NWheelerpi/Cabri/blob/a046b44fc315f65b8cebdb2e7d7a713d1c2d9b28/Parameters.txt.', delimiter='|',missing_values='NA')
 st.markdown(parameters)
