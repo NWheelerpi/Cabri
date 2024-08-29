36e2a85c7e4567b94d24b63dbd80ae860cf99c4a
NWheelerpi <156547753+NWheelerpi@users.noreply.github.com>
2024-08-29 15:03:41 +0100

    s

--- Code.py
+++ Code.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-# parameters = np.genfromtxt('https://github.com/NWheelerpi/Cabri/blob/a046b44fc315f65b8cebdb2e7d7a713d1c2d9b28/Parameters.txt.', delimiter='|',missing_values='NA')
-parameters = open('https://github.com/NWheelerpi/Cabri/blob/a046b44fc315f65b8cebdb2e7d7a713d1c2d9b28/Parameters.txt.','r')
+parameters = np.genfromtxt('https://github.com/NWheelerpi/Cabri/blob/a046b44fc315f65b8cebdb2e7d7a713d1c2d9b28/Parameters.txt.', delimiter='|')
+#parameters = open('https://github.com/NWheelerpi/Cabri/blob/a046b44fc315f65b8cebdb2e7d7a713d1c2d9b28/Parameters.txt.','r')
 st.markdown(parameters)
 parameters.close()
