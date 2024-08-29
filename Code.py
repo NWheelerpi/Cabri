964b64c5a82411725e15884b87c52b719ee1779e
NWheelerpi <156547753+NWheelerpi@users.noreply.github.com>
2024-08-29 15:44:17 +0100

    s

--- Code.py
+++ Code.py
@@ -8,9 +8,12 @@ actions=[]
 for item in parameters:
     if ' \ ' in item[1]:
         sub_actions=item[1].split(' \ ')
+        for i in sub_actions:
+            if i not in actions:
+                actions.append(i)
     else:
-        sub_actions=str(item[1])
-    actions.append(sub_actions)
+        if i not in actions:
+            actions.append(str(item[1]))
 
 for i in parameters:
     st.markdown(i)
