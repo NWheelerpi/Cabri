9b29f50ca6d4b43e54ebeed48734f070d82dea24
NWheelerpi <156547753+NWheelerpi@users.noreply.github.com>
2024-08-29 15:46:46 +0100

    a

--- Code.py
+++ Code.py
@@ -6,14 +6,14 @@ parameters = np.genfromtxt('https://raw.githubusercontent.com/NWheelerpi/Cabri/m
 
 actions=[]
 for item in parameters:
-    if ' \ ' in item[1]:
-        sub_actions=item[1].split(' \ ')
+    if ' \ ' in item[2]:
+        sub_actions=item[2].split(' \ ')
         for i in sub_actions:
             if i not in actions:
                 actions.append(i)
     else:
-        if item[1] not in actions:
-            actions.append(str(item[1]))
+        if item[2] not in actions:
+            actions.append(str(item[2]))
 
 for i in parameters:
     st.markdown(i)
