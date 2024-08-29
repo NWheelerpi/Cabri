d77b35204ec223e303b8161fc384505264ce641f
NWheelerpi <156547753+NWheelerpi@users.noreply.github.com>
2024-08-29 14:37:36 +0100

    d

--- Code.py
+++ Code.py
@@ -1,4 +1,4 @@
 import numpy as np
 
-parameters = np.genfromtxt('NWheelerpi/Cabri/main/Parameters.txt', delimiter='|',missing_values='NA')
+parameters = np.genfromtxt('Parameters.txt', delimiter='|',missing_values='NA')
 st.markdown(parameters)
