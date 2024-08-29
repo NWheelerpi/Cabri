9f87c7add504e47aba3951cd9b02c992cc29c124
NWheelerpi <156547753+NWheelerpi@users.noreply.github.com>
2024-08-29 15:37:29 +0100

    s

--- Code.py
+++ Code.py
@@ -4,6 +4,8 @@ import streamlit as st
 # Extract parameter data
 parameters = np.genfromtxt('https://raw.githubusercontent.com/NWheelerpi/Cabri/main/Parameters.txt.', delimiter='|',dtype=str)
 
-
+actions=[]
+for item in parameters:
+    if '\' in item 
 
 st.markdown(parameters[0])
