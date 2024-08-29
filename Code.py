3886bff97fecba1066a9695b257b00dd14637590
NWheelerpi <156547753+NWheelerpi@users.noreply.github.com>
2024-08-29 15:00:42 +0100

    s

--- Code.py
+++ Code.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import main.Parameters
 
 # parameters = np.genfromtxt('https://github.com/NWheelerpi/Cabri/blob/a046b44fc315f65b8cebdb2e7d7a713d1c2d9b28/Parameters.txt.', delimiter='|',missing_values='NA')
 parameters = open('https://github.com/NWheelerpi/Cabri/blob/a046b44fc315f65b8cebdb2e7d7a713d1c2d9b28/Parameters.txt.','r')
