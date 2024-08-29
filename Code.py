e32a632addcf788d56a2a87661e65966b1c47099
NWheelerpi <156547753+NWheelerpi@users.noreply.github.com>
2024-08-29 16:49:24 +0100

    s

--- Code.py
+++ Code.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 # Extract parameter data
 parameters = np.genfromtxt('https://raw.githubusercontent.com/NWheelerpi/Cabri/main/Parameters.txt.', delimiter='|',dtype=str)
-actions= np.genfromtxt('https://raw.githubusercontent.com/NWheelerpi/Cabri/main/actions.txt', delimiter='|',dtype=str)
+actions= np.genfromtxt('https://raw.githubusercontent.com/NWheelerpi/Cabri/main/actions.txt', delimiter='|',dtype=str,skip_header=1)
 
 st.markdown(actions)
 st.markdown(len(actions))
