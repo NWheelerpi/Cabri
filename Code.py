c637fca6a6a6432b8ad5e88ab9a48c5d4898f131
NWheelerpi <156547753+NWheelerpi@users.noreply.github.com>
2024-08-29 16:46:41 +0100

    d

--- Code.py
+++ Code.py
@@ -3,18 +3,8 @@ import streamlit as st
 
 # Extract parameter data
 parameters = np.genfromtxt('https://raw.githubusercontent.com/NWheelerpi/Cabri/main/Parameters.txt.', delimiter='|',dtype=str)
-actions= np.genfromtxt('https://raw.githubusercontent.com/NWheelerpi/Cabri/main/actions.txt', delimiter='|',dtype=str,filling_values=None)
-#
-#actions=[]
-#for item in parameters:
-#    if '\\' in item[2]:
-#        sub_actions=item[2].split('\\')
-#        for i in sub_actions:
-#            if i not in actions:
-#                actions.append(i)
-#    else:
-#        if item[2] not in actions:
-#            actions.append(str(item[2]))
+actions= np.genfromtxt('https://raw.githubusercontent.com/NWheelerpi/Cabri/main/actions.txt', delimiter='|',dtype=str)
+
 st.markdown(actions)
 st.markdown(len(actions))
 for item in parameters:
@@ -31,7 +21,7 @@ for item in parameters:
             pass
         #st.markdown(inp)
         acts+=str(actions[inp])
-    item[1]=acts
+    item[1]=acts[0]
     st.markdown(item)
 
 st.markdown(parameters)
