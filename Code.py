a046b44fc315f65b8cebdb2e7d7a713d1c2d9b28
NWheelerpi <156547753+NWheelerpi@users.noreply.github.com>
2024-08-29 14:42:30 +0100

    r

--- Code.py
+++ Code.py
@@ -1,4 +1,4 @@
 import numpy as np
 
-parameters = np.genfromtxt('Parameters.txt', delimiter='|',missing_values='NA')
+parameters = np.genfromtxt('repo/Parameters.txt', delimiter='|',missing_values='NA')
 st.markdown(parameters)
